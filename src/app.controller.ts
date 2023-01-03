b781abe79376d77787cfba5026a36f9961d66a5b
hrutik7 <hritikkumthekar7@gmail.com>
2023-01-03 10:58:02 +0530

    rest controll api created

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get ,Post,Put,Delete} from '@nestjs/common';
+// import { Post } from '@nestjs/common/decorators';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/findAll')
   getHello(): string {
-    return this.appService.getHello();
+    return 'This will get book'
+  }
+  @Post('/add')
+  PostHello(): string {
+    return 'This will post book'
+  }
+  @Delete('/delete')
+  DeleteHello(): string {
+    return 'This will delete book'
+  }
+  @Put('/update')
+  PutHello(): string {
+    return 'This will put book'
   }
 }
